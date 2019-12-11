4422a0c9d808414dd4bba61970bd624c4d9957d5
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-12-11 23:48:34 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+attr_accessor :name 
+attr_reader :species
+@@all = []
+
+def initialize(name, species = "human")
+  @name = name
+  @species = species
+  @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.count
+    @@all.size
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
 end
