614ec16c83922d0b6c3fa522166eaf9feaa3275a
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-12-12 13:23:29 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,15 @@
 class Owner
-attr_accessor :name 
-attr_reader :species
+  
+attr_accessor :owner  
+attr_reader :species, :name 
 @@all = []
 
 def initialize(name, species = "human")
   @name = name
   @species = species
   @@all << self 
-  end 
+  end
+  
   
   def self.all
     @@all 
