5d5e0c60d131f865be9664fd48571cfd68c0ff9e
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-12 09:06:46 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :owner, :mood
+  @@all=[]
+ def initialize(name, owner, mood="nervous")
+   @name=name
+   @owner=owner
+   @species="cat"
+   @mood=mood
+   @@all<<self
+ end
+ def self.all
+   @@all
+ end
 end
