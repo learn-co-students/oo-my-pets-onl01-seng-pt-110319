7a89f8cebbfeb81e68931fa6dc3b54f39655ba63
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-12-13 21:48:28 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,6 +41,25 @@ def initialize(name, species = "human")
     end 
   end 
   
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+  
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end 
+  
+  def feed_cats
+    self.cats.each {|cat|cat.mood = "happy"}
+  end 
+  
+  def sell pets 
+  end 
 end 
+
 
   
