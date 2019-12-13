49dc8a29292b251ca36409547afcf66f155996b8
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-12-13 21:34:13 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
   
-attr_accessor :owner   
+attr_accessor :owner, :cats, :dogs  
 attr_reader :species, :name
 @@all = []
 
@@ -8,21 +8,39 @@ def initialize(name, species = "human")
   @name = name
   @species = species
   @@all << self 
+  @cats = []
+  @dogs = []
   end
+
   
+   def self.all
+    @@all 
+  end 
+
   def say_species 
   "I am a #{self.species}."
   end 
   
-  def self.all
-    @@all 
-  end 
-  
-  def self.count
+   def self.count
     @@all.size
   end 
   
   def self.reset_all 
     @@all.clear
+  end
+
+    def cats	      
+    Cat.all.select do |cat|
+      cat.owner == self
+    end 
   end 
-end
+  
+  def dogs 
+    Dog.all.select do | dog|
+      dog.owner == self
+    end 
+  end 
+  
+end 
+
+  
